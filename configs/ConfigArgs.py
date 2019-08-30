cd13cde1df4e1019cfde477db7cae4e72963fa51
NakamoriAkina-TGS <jieshentan@163.com>
2019-08-31 04:40:06 +0800

    Support the arguments of program without values

--- configs/ConfigArgs.py
+++ configs/ConfigArgs.py
@@ -7,27 +7,34 @@ import argparse
 
 g_args_need = [{
         'args': ['-p', '--path'],
+        'action': None,
         'help': 'directory for searching picture'
     }, {
         'args': ['-f', '--file'],
+        'action': None,
         'help': 'sample picture used for searching'
     }
 ]
 
 g_args_optional = [{
         'args': ['-vvvvv'],
+        'action': 'store_true',
         'help': 'log level of error, info, warning, debug, verbose'
     }, {
         'args': ['-vvvv'],
+        'action': 'store_true',
         'help': 'log level of error, info, warning, debug'
     }, {
         'args': ['-vvv'],
+        'action': 'store_true',
         'help': 'log level of error, info, warning'
     }, {
         'args': ['-vv'],
+        'action': 'store_true',
         'help': 'log level of error, info'
     }, {
         'args': ['-v'],
+        'action': 'store_true',
         'help': 'log level of error'
     }
 ]
@@ -38,7 +45,13 @@ def config_args_parse():
     global g_args_optional
     ap = argparse.ArgumentParser()
     for arg in g_args_need:
-        ap.add_argument(*arg['args'], required=True, help=arg['help'])
+        if arg['action'] is not None:
+            ap.add_argument(*arg['args'], required=True, help=arg['help'], action=arg['action'])
+        else:
+            ap.add_argument(*arg['args'], required=True, help=arg['help'])
     for arg in g_args_optional:
-        ap.add_argument(*arg['args'], help=arg['help'])
+        if arg['action'] is not None:
+            ap.add_argument(*arg['args'], help=arg['help'], action=arg['action'])
+        else:
+            ap.add_argument(*arg['args'], help=arg['help'])
     return ap.parse_args()
