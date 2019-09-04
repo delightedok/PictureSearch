9ec37c1caa6e5fd4111cae827fce757ee0f3e508
Jieshen.Tan <jieshen.tan@legrand.com.cn>
2019-09-04 18:24:43 +0800

    Add picture info when analyse a picture

--- picture/PictureAnalyse.py
+++ picture/PictureAnalyse.py
@@ -15,14 +15,13 @@ def analyse_picture_in_directory(path, suffix_list):
     for suffix in suffix_list:
         for picture_path in glob.glob(path + '\*.' + suffix):
             # CommonLog.log_d(picture_path)
-            if suffix.lower() == 'png':
-                image = Image.open(picture_path).convert('RGBA')
-            else:
-                image = Image.open(picture_path)
-            image_hash = str(imagehash.dhash(image))
             fp_dict = dict()
+            image = Image.open(picture_path)
+            fp_dict[PictureCommons.PICTURE_COMMON_KEY_INFO] = PictureInfo.get_picture_info_by_image(image)
+            if suffix.lower() == 'png':
+                image = image.convert('RGBA')
+            image_hash = str(imagehash.dhash(image))
             fp_dict[PictureCommons.PICTURE_COMMON_KEY_FILENAME] = picture_path
             fp_dict[PictureCommons.PICTURE_COMMON_KEY_FINGERPRINT] = image_hash
-            fp_dict[PictureCommons.PICTURE_COMMON_KEY_INFO] = PictureInfo.get_picture_info_by_image(image)
             fp_list.append(fp_dict)
     return fp_list
