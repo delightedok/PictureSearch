bce6c28af4960a62543e265d0728b0d7f0f79e01
NakamoriAkina-TGS <jieshentan@163.com>
2019-09-08 22:51:31 +0800

    Update UI of tkinter

--- ui/UITkinter.py
+++ ui/UITkinter.py
@@ -4,6 +4,7 @@
 
 from tkinter import *
 import tkinter.messagebox
+from tkinter import ttk
 from Log import CommonLog
 
 
@@ -59,15 +60,43 @@ def _init_menus(root):
     menu_help.add_command(label='Help')
     menu_help.add_separator()
     menu_help.add_command(label='Check for Updates...')
-    menu_help.add_command(label='About', command=lambda : _show_about(root))
+    menu_help.add_command(label='About', command=lambda: _show_about(root))
     menu_bar = Menu(root)
     menu_bar.add_cascade(label='Help', menu=menu_help)
     root['menu'] = menu_bar
 
 
+def _init_ttk_styles():
+    style = ttk.Style()
+    style.configure('Content.TFrame', background=g_attributes['background'])
+    style.configure('Label.TLabel', background=g_attributes['background'], foreground='white')
+
+
+def _init_main_view(root):
+    global g_attributes
+    content = ttk.Frame(root, padding=(10, 10, 10, 10), style='Content.TFrame')
+
+    label_file = ttk.Label(content, text='File:', style='Label.TLabel', font=('Times New Roman', 12, 'normal'))
+    label_path = ttk.Label(content, text='Path:', style='Label.TLabel', font=('Times New Roman', 12, 'normal'))
+    label_database = ttk.Label(content, text='Database:', style='Label.TLabel', font=('Times New Roman', 12, 'normal'))
+
+    label_file.grid(row=0, column=0, sticky=W)
+    content.columnconfigure(0, minsize=15)
+    label_path.grid(row=1, column=0, sticky=W)
+    content.columnconfigure(1, minsize=15)
+    label_database.grid(row=2, column=0, sticky=W)
+    content.columnconfigure(2, minsize=15)
+
+    content.grid(row=0, column=0, sticky=(N, S, E, W))
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+
+
 def _init_views(root):
     _set_attributes(root)
+    _init_ttk_styles()
     _init_menus(root)
+    _init_main_view(root)
     pass
 
 
